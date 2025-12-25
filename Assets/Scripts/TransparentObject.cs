47a90e69d680d7324fe3fe3e6dfe0c88a7d3b903
Myrella669 <myrella.santos@ufv.br>
2025-12-25 18:44:14 -0300

    Efeito de Transparência

--- Assets/Scripts/TransparentObject.cs
+++ Assets/Scripts/TransparentObject.cs
@@ -1,16 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TransparentObject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Range(0, 1)]
+    [SerializeField] private float _transparencyValue = 0.7f;
+    [SerializeField] private float _transparencyFadeTime = .4f;
+
+    private SpriteRenderer _spriteRenderer;
+
+    void Awake()
     {
-        
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+        if (collision.gameObject.GetComponent<playerController>())
+        {
+            StartCoroutine(FadeTree(_spriteRenderer, _transparencyFadeTime, _spriteRenderer.color.a, _transparencyValue));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
     {
         
+        if (collision.gameObject.GetComponent<playerController>())
+        {
+            StartCoroutine(FadeTree(_spriteRenderer, _transparencyFadeTime, _spriteRenderer.color.a, 1f));
+        }
+    }
+
+    private IEnumerator FadeTree(SpriteRenderer _sprite, float _fadeTime, float _startValue, float _targetTransparency)
+    {
+        float _timeElapsed = 0;
+
+        while (_timeElapsed < _fadeTime)
+        {
+            _timeElapsed += Time.deltaTime;
+            float _newAlpha = Mathf.Lerp(_startValue, _targetTransparency, _timeElapsed / _fadeTime);
+
+            _sprite.color = new Color(_sprite.color.r, _sprite.color.g, _sprite.color.b, _newAlpha);
+
+            yield return null;
+        }
     }
 }
